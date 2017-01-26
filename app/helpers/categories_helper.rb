b73292e11ce02556677309f6609bd93dc573c372
theserialhobbyist <coward.cameron@gmail.com>
2017-01-26 14:30:30 -0700

    UI and form changes

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -1,5 +1,5 @@
 module CategoriesHelper
   def category_params
-    params.require(:category).permit(:cat_name, :cat_budget)
+    params.require(:category).permit(:cat_name, :cat_budget, :cat_average)
   end
 end
